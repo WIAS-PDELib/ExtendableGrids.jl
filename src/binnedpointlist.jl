ea7a6e284d127be5d1ce8efcdb6ac9758b29e8e1
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2026-09-15 11:12:22 +0200

    Merge pull request #137 from WIAS-PDELib/jf/reltolbinning
    Use relative tolerance in bin increase

--- src/binnedpointlist.jl
+++ src/binnedpointlist.jl
@@ -193,9 +193,13 @@ function _rebin_all_points!(bpl)
             bpl.binning_region_min[i] = min(bpl.binning_region_min[i], e[i][1])
             bpl.binning_region_max[i] = max(bpl.binning_region_max[i], e[i][2])
 
+            reltol = bpl.tol * (abs(bpl.binning_region_max[i]) + abs(bpl.binning_region_min[i])) / 2
+            if reltol == 0.0
+                reltol = bpl.tol
+            end
             # Slightly increase binning region further in order to
             # include all existing points with tolerance
-            delta = max(bpl.binning_region_max[i] - bpl.binning_region_min[i], bpl.tol)
+            delta = max(bpl.binning_region_max[i] - bpl.binning_region_min[i], reltol)
             bpl.binning_region_min[i] -= bpl.binning_region_increase_factor * delta
             bpl.binning_region_max[i] += bpl.binning_region_increase_factor * delta
         end
